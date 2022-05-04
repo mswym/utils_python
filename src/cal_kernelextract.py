ded346669c889aa9130cfeeafde4120fdce8652d
mswym <masa.sawayama@gmail.com>
2022-05-04 18:15:32 +0200

    Update files

--- src/cal_kernelextract.py
+++ src/cal_kernelextract.py
@@ -6,47 +6,51 @@ import glob
 import pickle
 import matplotlib.pyplot as plt
 
-from sklearn.decomposition import PCA, FastICA
+from sklearn.decomposition import PCA, FastICA, DictionaryLearning
 
 from load_imgs import *
 
-def cal_white_pca(imgs):
 
-    return imgs
+def cal_norm(img):
+    mean_imgs = np.mean(imgs)
+    std_imgs = np.std(imgs)
+    imgs_norm = (imgs - mean_imgs)/std_imgs
+
+    return imgs_norm, mean_imgs, std_imgs
+
+def cal_ica(imgs, num_components):
+    ica = FastICA(num_components)
+    ica.fit(imgs)
+
+    return ica
+
+def cal_dictionary(imgs, num_components):
+    dict_learner = DictionaryLearning(n_components=num_components, transform_algorithm='lasso_lars')
+    dict_learner.fit(imgs)
+
+    return ica
 
 if __name__ == '__main__':
-    path = '../img_dif_cut/cut_imgs_layer6-8_256.pickle'
+    path = '../img_dif_cut/cut_imgs_layer6-8_170.pickle'
     num_components = 25
 
     with open(path, mode='rb') as f:
         imgs = pickle.load(f)
 
     imgs = np.array(imgs)
-
+    imgs = imgs[0:1000,:,:,:]
     size_imgs = imgs.shape
 
     #reshape
     imgs = np.reshape(imgs,[size_imgs[0], size_imgs[1]*size_imgs[2]*size_imgs[3]])
     #normalize
-    mean_imgs = np.mean(imgs)
-    std_imgs = np.std(imgs)
-    imgs_norm = (imgs - mean_imgs)/std_imgs
+    imgs_norm, mean_imgs, std_imgs = cal_norm(imgs)
 
-    #whitenening
-    pca = PCA(num_components)
-    pca.fit(imgs_norm)
-
-    print('cumulated percentage is...')
-    print(np.cumsum(pca.explained_variance_ratio_))
-
-    print(pca.components_.shape) #25*768
-    imgs_white = np.dot(pca.components_, imgs_norm.T)
-
-    ica = FastICA(num_components, max_iter=1000)
-    ica.fit(imgs_white.T)
-    Uica = ica.mixing_
-    print(Uica.shape) #pcanum x icanum
-
-    kernel = np.dot(pca.comsponents_.T,Uica[:, 0])
-    kernel = np.reshape(kernel,[size_imgs[1], size_imgs[2], size_imgs[3]])
+    #ica
+    ica = cal_ica(imgs_norm, num_components)
+    sparse = cal_dictionary(imgs_norm, num_components)
+    #reshape and rescale
+    kernel = np.reshape(ica.components_,[num_components, size_imgs[1], size_imgs[2], size_imgs[3]])
     kernel = (kernel*std_imgs)+mean_imgs
+
+    a = 1
