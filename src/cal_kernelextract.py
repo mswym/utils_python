a2d3a21ba86316a7918054857b8a4929f8bfb607
mswym <masa.sawayama@gmail.com>
2022-05-04 18:55:18 +0200

    Update kernel extractions

--- src/cal_kernelextract.py
+++ src/cal_kernelextract.py
@@ -10,6 +10,17 @@ from sklearn.decomposition import PCA, FastICA, DictionaryLearning
 
 from load_imgs import *
 
+def sample_roi(imgs, size_cut, num_patches_per_img):
+
+    roi = []
+    for ind_img in range(imgs.shape[0]):
+        for ind_patches in range(num_patches_per_img):
+            x_start = np.random.randint(0, imgs.shape[2] - size_cut - 1)
+            y_start = np.random.randint(0, imgs.shape[1] - size_cut - 1)
+            roi.append(np.reshape(imgs[ind_img, y_start: y_start + size_cut, 
+                                  x_start: x_start + size_cut, :],
+                                  [size_cut, size_cut, 3]))
+    return np.array(roi)
 
 def cal_norm(img):
     mean_imgs = np.mean(imgs)
@@ -30,17 +41,26 @@ def cal_dictionary(imgs, num_components):
 
     return ica
 
-if __name__ == '__main__':
-    path = '../img_dif_cut/cut_imgs_layer6-8_170.pickle'
-    num_components = 25
-
+def cal_sequential_sample(path):
     with open(path, mode='rb') as f:
         imgs = pickle.load(f)
 
     imgs = np.array(imgs)
-    imgs = imgs[0:1000,:,:,:]
+    imgs = imgs[0:1000, :, :, :]
     size_imgs = imgs.shape
 
+if __name__ == '__main__':
+    path = '../img_dif170/imgs_layer6-8_170.pickle'
+    num_components = 100
+    num_patches_per_img = 1
+    size_cut = 16
+
+    with open(path, mode='rb') as f:
+        imgs = pickle.load(f)
+        imgs = np.array(imgs)
+
+    imgs = sample_roi(imgs, size_cut, num_patches_per_img)
+    size_imgs = imgs.shape
     #reshape
     imgs = np.reshape(imgs,[size_imgs[0], size_imgs[1]*size_imgs[2]*size_imgs[3]])
     #normalize
@@ -48,9 +68,11 @@ if __name__ == '__main__':
 
     #ica
     ica = cal_ica(imgs_norm, num_components)
-    sparse = cal_dictionary(imgs_norm, num_components)
+    # sparse = cal_dictionary(imgs_norm, num_components)
     #reshape and rescale
-    kernel = np.reshape(ica.components_,[num_components, size_imgs[1], size_imgs[2], size_imgs[3]])
+    kernels = np.reshape(ica.components_,[num_components, size_imgs[1], size_imgs[2], size_imgs[3]])
+    kernels = kernels - np.min(kernels)
+    kernels = kernels/np.max(kernels)
     kernel = (kernel*std_imgs)+mean_imgs
 
     a = 1
