14cee1bb4a7e8a63985d46ee19a824c951c3b64b
mswym <masa.sawayama@gmail.com>
2022-07-28 18:44:32 +0200

    Update make_dataloader

--- src/make_dataloader_from_imagelist.py
+++ src/make_dataloader_from_imagelist.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 #prepared this for TensorFlow Hub demos because I could not use tf.data.Dataset
 
@@ -30,6 +31,21 @@ def make_ds_from_imglist(path_imglist, size_batch, size_img=None):
     # dataset = dataset.batch(size_batch)
     # list(dataset.as_numpy_iterator())
 
+  return imgs
+
+def make_ds_from_veclist(path_veclist, size_batch):
+  vecs = []
+  batch = []
+
+  for i,vec in enumerate(path_veclist):
+    batch.append(np.array(vec).astype('float32'))
+
+    if i==len(path_veclist)-1 or len(batch) == size_batch:
+      batch = np.array(batch) / (255 / 2) - 1  # 0-255 to -1 to 1 #for pretrained bigGAN
+      vecs.append(batch)
+      batch = []
+
+  return vecs
 
 if __name__ == '__main__':
   size_img = (256,256)
